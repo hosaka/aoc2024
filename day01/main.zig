9c5f719ae315f6e1cc70926afaee47831f13834b
Alex March <alexmarch@fastmail.com>
2024-12-03 00:27:07 +0900

    Initial commit

---
+++ day01/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+pub fn solve(input: []const u8) u32 {
+    _ = std.mem.split(u8, input, "\n");
+    return 42;
+}
+
+pub fn main() !void {
+    // // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // // stdout is for the actual output of your application, for example if you
+    // // are implementing gzip, then only the compressed bytes should be sent to
+    // // stdout, not any debugging messages.
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // try bw.flush(); // don't forget to flush!
+}
+
+test "test input" {
+    const distance = solve(@embedFile("test.txt"));
+    try std.testing.expectEqual(distance, 42);
+}
